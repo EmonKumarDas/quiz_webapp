911dc32b59c66a41a43eadfc894751ea40f15499
emon <emondas457@gmail.com>
2022-10-11 01:35:23 +0600

    option button set

--- src/Components/quizcard/QuizCard.js
+++ src/Components/quizcard/QuizCard.js
@@ -1,6 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const QuizCard = ({ question, name, options }) => {
+    let handleInput = e => {
+        const buttonValue = e.target.value;
+        console.log(buttonValue);
+
+  }
     return (
         <div className="card w-full bg-base-100 shadow-sm mb-5 ">
             <div className="card-body">
@@ -8,7 +13,7 @@ const QuizCard = ({ question, name, options }) => {
                 <h2 className="card-title">Questions:- {question}!</h2>
                 <div className=" mt-5 card-actions justify-start grid lg:grid-cols-2 md:grid-cols-2 mx-[20%] lg:mx-0 md:mx-0">
                     {
-                        options.map(option => <button className="btn btn-primary">
+                        options.map(option => <button onClick={e => handleInput(e, "value")} className="btn btn-primary" value={option}>
                             {option}
                         </button>)
                     }
