e43559938e0a435b065fbc994bd5bb3bd4b58679
emon <emondas457@gmail.com>
2022-10-11 01:13:51 +0600

    questions options set

--- src/Components/Quiz.js
+++ src/Components/Quiz.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
-
+import QuizCard from '../Components/quizcard/QuizCard'
 const Quiz = () => {
     const quiz = useLoaderData();
-    console.log(quiz);
+console.log(quiz.data.questions[0].options);
     return (
-        <div>
-            <h1>quiz{quiz.data.total}</h1>
+        <div className="m-5 gap-5">
+            {
+                quiz.data.questions.map(question=><QuizCard 
+                    key={quiz.data.id}
+                    options={question.options}
+                    name={quiz.data.name}
+                    question={question.question}></QuizCard>)
+            }
         </div>
     );
 };
--- src/Components/quizcard/QuizCard.js
+++ src/Components/quizcard/QuizCard.js
@@ -1,9 +1,20 @@
 import React from 'react';
 
-const QuizCard = () => {
+const QuizCard = ({ question, name, options }) => {
     return (
-        <div>
-            
+        <div className="card w-full bg-base-100 shadow-xl mb-5 ">
+            <div className="card-body">
+                <h2 className="card-title mx-[50%] text-3xl">{name}!</h2>
+                <h2 className="card-title">Questions:- {question}!</h2>
+                <div className="card-actions justify-start grid grid-cols-2">
+                    {
+                        options.map(option => <button className="btn btn-primary">
+                            {option}
+                        </button>)
+                    }
+
+                </div>
+            </div>
         </div>
     );
 };
