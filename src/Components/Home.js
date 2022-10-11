886326a274c28955295f68988883ee60dab0abd4
emon <emondas457@gmail.com>
2022-10-11 21:19:59 +0600

    icon set header

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -11,8 +11,13 @@ const Home = () => {
   }
   return (
     <div>
-      <h1 className="font-bold text-4xl my-5">✍ Which Best Matches Your Skill Prefernce?</h1>
+      <div className="flex-row-reverse mx-[14%] mt-5">
+        <img className="w-[10%] h-[10%] mx-[50%]" src="https://e7.pngegg.com/pngimages/696/845/png-clipart-pub-quiz-test-quiz-bowl-flashcard-button-miscellaneous-game-thumbnail.png" alt="" srcset="" />
+        <h1 className="font-bold text-4xl my-5">✍ Which Best Matches Your Skill Prefernce?</h1>
+        
+      </div>
       <p>This application is developed for educational purpose, allowing the users to prepare the multiple choice questions for different examinations conducted on provincial and national level.</p>
+     
       <div className='grid lg:grid-cols-3 md:grid-cols-2 gap-5 m-5'>
       {
         data.map(quiz=><Cart getid={getid} key={quiz.id} quiz={quiz}></Cart>)
