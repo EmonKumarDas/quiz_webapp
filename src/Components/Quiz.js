224d6ef9fc96f40c49cf65fb1b64f3ee62be12e0
emon <emondas457@gmail.com>
2022-10-11 10:49:34 +0600

    set score

--- src/Components/Quiz.js
+++ src/Components/Quiz.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
 import QuizCard from '../Components/quizcard/QuizCard'
+
 const Quiz = () => {
+    let [correct,setCorrect] = useState(0);
+    let scores =(num)=>{
+        setCorrect(num);
+        let newvalue = correct+1;
+        setCorrect(newvalue);
+    }
     const quiz = useLoaderData();
 console.log(quiz.data.questions[0].options);
     return (
         <div className="m-5 gap-5">
+            <h1 className='bg-sky-900 p-5 rounded font-bold text-xl text-white'>Your Score is : {correct}/{quiz.data.questions.length}</h1>
             {
                 quiz.data.questions.map(question=><QuizCard 
+                    score={scores}
                     options={question.options}
                     key = {question.id}
                     name={quiz.data.name}
--- src/Components/quizcard/QuizCard.js
+++ src/Components/quizcard/QuizCard.js
@@ -1,8 +1,11 @@
-import React, { } from 'react';
+
+import React, { useState } from 'react';
 import {toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-const QuizCard = ({ question, name, options,correctAnswer}) => {
-    console.log(correctAnswer);
+const QuizCard = ({ question, name, options,correctAnswer,score}) => {
+
+    let [disable,setdisable] = useState(false);
+
     let handleInput = e => {
         const buttonValue = e.target.value;
         if(buttonValue===correctAnswer){
@@ -11,6 +14,8 @@ const QuizCard = ({ question, name, options,correctAnswer}) => {
                 position: toast.POSITION.TOP_RIGHT,
                 autoClose:1000
             });
+            score(1);
+            setdisable(true);
 
         }
         else{
@@ -18,6 +23,7 @@ const QuizCard = ({ question, name, options,correctAnswer}) => {
             toast.warning('Incorrect answer !', {
                 position: toast.POSITION.TOP_RIGHT
             });
+            setdisable(true);
         }
 
   }
@@ -28,7 +34,7 @@ const QuizCard = ({ question, name, options,correctAnswer}) => {
                 <h2 className="card-title">Questions:- {question}!</h2>
                 <div className=" mt-5 card-actions justify-start grid lg:grid-cols-2 md:grid-cols-2 mx-[20%] lg:mx-0 md:mx-0">
                     {
-                        options.map(option => <button onClick={e => handleInput(e, "value")} className="btn btn-primary" value={option}>
+                        options.map(option => <button disabled={disable} onClick={e => handleInput(e, "value")} className="btn btn-outline" value={option}>
                             {option}
                         </button>)
                     }
