d82f939c20acf536ed3366091df0b542e2e19cf7
emon <emondas457@gmail.com>
2022-10-11 11:15:26 +0600

    404 style

--- src/Components/ErrorPage.js
+++ src/Components/ErrorPage.js
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom';
 
 const ErrorPage = () => {
     return (
-        <div>
-            <img className="mx-[25%] my-[2%] w-[50%] rounded" src="https://img.freepik.com/free-vector/404-error-with-portals-concept-illustration_114360-7870.jpg?w=740&t=st=1665464183~exp=1665464783~hmac=73fa4242a37d71c605cdecedac5b313fbf6f5896623ef7dacfbcf4b9c57ed8a9" alt="" srcset="" />
-        <button className='bg-green-900 text-white font-bold w-[10%] p-5 rounded'><Link to={"/home"}> Go Back</Link></button>
+        <div  className="lg:my-[2%] md:my-[30%] my-[50%]">
+            <img className="lg:mx-[25%] md:mx-[25%] mx-[14%] lg:w-[50%] md:w-[50%] w-[70%] rounded" src="https://img.freepik.com/free-vector/404-error-with-portals-concept-illustration_114360-7870.jpg?w=740&t=st=1665464183~exp=1665464783~hmac=73fa4242a37d71c605cdecedac5b313fbf6f5896623ef7dacfbcf4b9c57ed8a9" alt="" srcset="" />
+        <button className='bg-green-900 mt-5 text-white font-bold p-5 rounded'><Link to={"/home"}> Go Back</Link></button>
         </div>
     );
 };
